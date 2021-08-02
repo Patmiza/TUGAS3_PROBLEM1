7bc6c31dbaa3d939b34c9821fc6f9a3eff6e3e63
Patmiza <patmizanopiani@gmail.com>
2021-08-02 13:49:23 +0700

    first commit branch

--- main.go
+++ main.go
@@ -98,6 +98,7 @@ func UpdateUserController(c echo.Context) error {
 // ---------------------------------------------------
 func main() {
 	//latihan first commit
+	//latihan branch
 	users = []User{}
 	e := echo.New()
 	//e.Server.ListenAndServe()
